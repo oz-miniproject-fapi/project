914012b2cd208c5be833baf414238921e5c31852
zlfktm <a50639174@gmail.com>
2025-09-01 20:54:23 -0700

    Delete app/services directory

--- app/services/tag_service.py
+++
@@ -1,8 +0,0 @@
-from app.repositories.tag_repo import TagRepository
-
-class TagService:
-    def __init__(self):
-        self.tag_repo = TagRepository()
-
-    async def list_tags(self):
-        return await self.tag_repo.list_all()
--- app/services/token_service.py
+++
@@ -1,9 +0,0 @@
-from app.repositories.token_repo import TokenRepository
-
-class TokenService:
-    def __init__(self):
-        self.token_repo = TokenRepository()
-
-    async def blacklist_token(self, token: str):
-        await self.token_repo.add(token)
-        return {"message": "로그아웃 완료"}
